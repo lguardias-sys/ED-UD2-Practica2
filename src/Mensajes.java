1e87bb776959407cb92cb63df8260ad28e1092d5
Alonso <asorianom2007@gmail.com>
2025-12-05 20:56:27 +0100

    Cambio en mi metodo

--- src/Mensajes.java
+++ src/Mensajes.java
@@ -72,7 +72,7 @@ public class Mensajes{
         return "dos";
     }
     public String metodo03 () {
-        return "tres";
+        return "Hector Criado Butanero";
     }
     public String metodo04 () {
         return "Hola mundo, soy Héctor !!!";
